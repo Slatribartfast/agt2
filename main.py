8209fd0e7fc9f3a93c36d3db73d24a111bb898e6
Moritz Seefeldt <moritzseefeldt@yahoo.de>
2022-07-30 14:43:54 +0200

    moved parameter study and parallelization code to test_params.py

--- main.py
+++ main.py
@@ -1,95 +1,8 @@
-
-import time
-
 import staaten
 import loosingKoalition
-import calculateLoosingKoalition
 
-from cover import get_cover
+import test_params
 
-from multiprocessing import Process
-
-def a_process_for_parallel_execution(name ="file.txt", runtime = 3600, id = -1, max_out = 5, max_overall: int = 15, min_gain: int = 6, kick_strategy:str = "random", kick_rate: int = 100, give_up_rate: int = 1000, min_avg_pop_size = 0.645,min_pop_size=0.62):  
-    print("go")
-    search_space = calculateLoosingKoalition.get_search_space(max_out, min_pop_size)
-    best = 0
-    tries = 0 
-    start_time = time.time()
-    points = 0
-    # endlosschleife immer get loosing and get over nacheinander und ab 5 in datei
-    # ToDo aufteilen mit variablen      
-    while(time.time() < start_time + runtime):  
-        tries += 1
-        if True:
-            if tries % 10 == 0:
-                pass
-                # print(f"id: {id}, tries: {tries}")
-        lk = calculateLoosingKoalition.get_loosing_coalition(loosing_coals_all = search_space, max_out = max_out, max_overall = max_overall, min_gain = min_gain, kick_strategy = kick_strategy, kick_rate = kick_rate, give_up_rate = give_up_rate, min_avg_pop_size = min_avg_pop_size)
-        res = get_cover(lk = lk, debug = False)
-        if res > 4:
-            if res == 5:
-                points += 1
-            elif res == 6:
-                points += 100
-            best = res
-            print(best)
-            with open(name, 'a') as f: 
-                f.write(str(best)+ "\n")
-                f.write(f"process id {id}\n")
-                f.write("stats\n")
-                f.write(f"{max_out}, {max_overall}, {min_gain}, {kick_rate}, {give_up_rate}\n")
-                f.write(str(time.strftime("%Y%m%d-%H%M%S")))
-                f.write("\nloosing coalitions taken\n")
-                for elem in lk:
-                    f.write(str(staaten.make_readable_alla_paper(elem)) + "\n")
-                f.write("\n")
-                f.write("\n")
-                f.flush()
-
-def test_params(max_overall_params: list[int],
-                min_avg_pop_params: list[float],
-                min_pop_params: list[float],
-                runtime = 1800
-                ):
-    n = len(max_overall_params)
-    if not (n == len(min_avg_pop_params) == len(min_pop_params)):
-        print("Error: Parameter lists differ in length!")
-        return
-
-    for i in range(n):
-        proc = []
-
-        # default settings
-        num_processes = 16
-        max_out_l = [5] * num_processes
-        min_gain_l = [7] * num_processes
-        kick_strategy_l = ["random"] * num_processes
-        kick_rate_l = [100] * num_processes
-        give_up_rate_l = [1000] * num_processes
-
-        # assign parameters to study
-        max_overall_l = [max_overall_params[i]] * num_processes
-        min_avg_pop_size_l = [min_avg_pop_params[i]] * num_processes
-        min_pop_size_l = [min_pop_params[i]] * num_processes
-
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        name = "p_" + str(i) + "_" + timestr + ".txt"
-        with open(name, 'w') as f:
-            f.write("Results\n")
-            f.write("\nmax_overall: " + str(max_overall_l[0]))
-            f.write("\nmin_avg_pop_size: " + str(min_avg_pop_size_l[0]))
-            f.write("\nmin_pop_size: " + str(min_pop_size_l[0]) + "\n\n")
-            f.flush()
-
-        for i in range(num_processes):
-            proc.append(Process(target=a_process_for_parallel_execution, args=(
-                name, runtime, i, max_out_l[i], max_overall_l[i], min_gain_l[i], kick_strategy_l[i], kick_rate_l[i],
-                give_up_rate_l[i], min_avg_pop_size_l[i], min_pop_size_l[i])))
-            proc[-1].start()
-            time.sleep(0.1)
-        for i in range(len(proc)):
-            proc[i].join()
-    
 if __name__ == '__main__':
     
     for elem in loosingKoalition.loosing_coalitions:
@@ -103,10 +16,13 @@ if __name__ == '__main__':
     #a_process_for_parallel_execution()
     
     if True: 
-        runtime = 30
-        max_overall_params = [15, 14, 15, 14, 15, 14, 15, 14, 15, 14]
-        min_avg_pop_params = [0.64, 0.64, 0.64, 0.64, 0.64, 0.64, 0.641, 0.641, 0.641, 0.641]
-        min_pop_params = [0.636, 0.636, 0.637, 0.637, 0.638, 0.638, 0.639, 0.639, 0.64, 0.64]
+        runtime = 120
+        max_overall_params = [15, 15, 15, 15, 15, 15, 15, 15, 15, 15]
+        #min_avg_pop_params = [0.64, 0.64, 0.64, 0.64, 0.64, 0.64, 0.64, 0.64, 0.64, 0.64]
+        #min_pop_params = [0.63, 0.631, 0.632, 0.633, 0.634, 0.635, 0.636, 0.637, 0.638, 0.639]
+        min_avg_pop_params = [0.635, 0.636, 0.637, 0.638, 0.639, 0.64, 0.641, 0.642, 0.643, 0.644]
+        min_pop_params = [0.633] * 10
 
-        test_params(max_overall_params, min_avg_pop_params, min_pop_params, runtime)
+        #param_ids: "max_overall", "min_avg_pop", "min_pop"
+        test_params.test("min_avg_pop",max_overall_params, min_avg_pop_params, min_pop_params, runtime)
 
